a20167ab68d6bc51538ce9fd1405873b00ddc59e
Andras Kelemen <andras.kelemen84@gmail.com>
2021-09-27 15:59:23 +0100

    finished project v1.0

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuHamburger = document.querySelector('.menu-ham');
+const menuX = document.querySelector('.menu-x');
+const navList = document.querySelector('.nav-list');
+const backdrop = document.querySelector('.backdrop');
+const logo = document.querySelector('.logo');
+const btnLeft = document.querySelector('.slider-left');
+const btnRight = document.querySelector('.slider-right');
+const heroLeftSlides = document.querySelectorAll('.hero-left');
+const heroRightSlides = document.querySelectorAll('.hero-right');
+
+//Menu functions
+const toggleMenu = function () {
+  logo.classList.toggle('hidden');
+  menuHamburger.classList.toggle('opacity-0');
+  menuX.classList.toggle('opacity-0');
+  navList.classList.toggle('scale-0');
+  backdrop.classList.toggle('hidden');
+};
+
+//Slider functions
+
+let currentSlide = 0;
+const maxSlides = heroLeftSlides.length - 1;
+
+const goToSlide = function (slide) {
+  heroLeftSlides.forEach((s, i) => {
+    s.style.transform = `translateX(${(i - slide) * 100}%)`;
+  });
+  heroRightSlides.forEach((s, i) => {
+    s.style.transform = `translateX(${(i - slide) * 100}%)`;
+  });
+};
+
+const nextSlide = function () {
+  if (currentSlide === maxSlides) currentSlide = 0;
+  else currentSlide++;
+  goToSlide(currentSlide);
+};
+
+const prevSlide = function () {
+  if (currentSlide === 0) currentSlide = maxSlides;
+  else currentSlide--;
+  goToSlide(currentSlide);
+};
+
+const init = function () {
+  menuIcon.addEventListener('click', toggleMenu);
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+};
+init();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./**/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
@@ -30,10 +30,36 @@ module.exports = {
     maxHeight: {
       custom: '850px',
     },
+    height: {
+      small: '1102px',
+      medium: '813px',
+      large: '813px',
+      xlarge: '1085px',
+      '2xlarge': '461px',
+      '3xlarge': '583px',
+      28: '7rem',
+      full: '100%',
+    },
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       gridTemplateColumns: {
         xlhero: '725px auto',
-        hero: '965px auto',
+        hero: '917px auto',
         section: ' auto 650px auto',
       },
       gridTemplateRows: {
